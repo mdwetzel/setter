38de7f88ee4c0ae2f724ec9f71ac3b2f2887c8af
Mark Wetzel <mark@markwetzel.com>
2013-10-15 23:02:54 -0400

    Added form and some style.

--- setter.rb
+++ setter.rb
@@ -1,5 +1,44 @@
 require 'sinatra'
 
 get '/' do
+  erb :form
+end
+
+post '/' do
+  squat         = params[:squat]
+  benchpress    = params[:bench]
+  military      = params[:military]
+  row           = params[:row]
+  deadlift      = params[:deadlift]
   
+  @params = params
+  
+  @weights = {
+    squat:    calculate(squat),
+    bench:    calculate(benchpress),
+    military: calculate(military),
+    row:      calculate(row),
+    deadlift: calculate(deadlift)
+  }
+  
+  erb :form
+end
+
+private
+
+def calculate(working_weight)
+  
+  weight60 = working_weight * 0.6
+  
+  sets = 
+  [
+    { reps: 5, weight: 45 },
+    { reps: 5, weight: 45 },
+    { reps: 5, weight: working_weight.to_f * 0.4 },
+    { reps: 3, weight: working_weight.to_f * 0.6 },
+    { reps: 2, weight: working_weight.to_f * 0.8 },
+    { reps: 5, weight: working_weight },
+    { reps: 5, weight: working_weight },
+    { reps: 5, weight: working_weight }
+  ]
 end
